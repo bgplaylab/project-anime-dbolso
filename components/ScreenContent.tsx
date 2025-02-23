461cf47bf775157f3489508f30bb672ddd08fdc5
bgplaylab <bgplaylab@gmail.com>
2025-02-23 14:42:30 -0300

    integration supabase

--- components/ScreenContent.tsx
+++ components/ScreenContent.tsx
@@ -1,4 +1,6 @@
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import { supabase } from '../utils/supabase';
 
 import { EditScreenInfo } from './EditScreenInfo';
 
@@ -9,12 +11,39 @@ type ScreenContentProps = {
 };
 
 export const ScreenContent = ({ title, path, children }: ScreenContentProps) => {
+  const [todos, setTodos] = useState<any>([]);
+
+  useEffect(() => {
+    const getTodos = async () => {
+      try {
+        const { data: todos, error } = await supabase.from('todos').select();
+
+        if (error) {
+          console.error('Error fetching todos:', error.message);
+          return;
+        }
+
+        if (todos && todos.length > 0) {
+          setTodos(todos);
+        }
+      } catch (error:any) {
+        console.error('Error fetching todos:', error.message);
+      }
+    };
+
+    getTodos();
+  }, []);
+
   return (
     <View className={styles.container}>
-      <Text className={styles.title}>{title}</Text>
-      <View className={styles.separator} />
-      <EditScreenInfo path={path} />
-      {children}
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Todo List</Text>
+        <FlatList
+          data={todos}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => <Text key={item.id}>{item.title}</Text>}
+        />
+      </View>
     </View>
   );
 };
